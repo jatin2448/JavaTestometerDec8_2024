8400321e31f8a8021b3262e8a1f2d98b172c9120
jatin2448 <jatinpatel.j@gmail.com>
2024-12-18 08:55:29 +0530

    Add the comment in the TreeMapInJava

--- JavaPractice/src/main/java/Map/TreeMapInJava.java
+++ JavaPractice/src/main/java/Map/TreeMapInJava.java
@@ -7,6 +7,7 @@ public class TreeMapInJava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//This is the entry
 
 		Map<String, String> hmap = new TreeMap<>();
 		
